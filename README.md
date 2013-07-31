216691cc1f95cebbc33f67a524fc7626e579b792
Adam Smith <hellfire@servfire.net>
2013-07-31 18:26:27 +1000

    Tweaked README.md. Added Teensy++ 2.0 schematic. Added sample time for pump cell PID. Tweaked PID values for pump and heater. Tweaked infinite "air" reading down to 4.99 (more scale on graphs). Added linear lambda wideband output with configurable +5v value.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ WidebandO2
 
 An Arduino sketch for controlling a Bosch LSU 4.9 Wideband Lambda sensor with minimal parts
 
+My current circuit is based off the Teensy++ 2.0 (AT90USB based), but have plans to port it to something more Arduino-y sometime in the future with adjustable pinouts.
 
 Required Libraries
 ------------------
--- WidebandO2.ino
+++ WidebandO2.ino
@@ -9,10 +9,11 @@
 */
 
 
-#define BOARD_TEENSY_2PP
+#define BOARD_TEENSYPP20
 
 
 const float targetLambda = 1.0; // Narrowband simulation, cross point in lambda
+const float widebandTop = 2.0; // Where the +5v of the linear wideband output is
 
 const int initialRampUpTill = 255 / 14.0 * 7.5; // Heater warm up stage, this sets the top (TODO: Measure input voltage, max output should be 7.5v)
 const int initialRampUpStep = 255 / (14.0 / 0.5); // Heater warm up stage, this is how fast the voltage ramps up per second, spec sheet says .5v a second).
@@ -20,6 +21,9 @@ const int initialRampUpStep = 255 / (14.0 / 0.5); // Heater warm up stage, this 
 double heatTarget = 2.3 / 1300.0 * 200.0; // Target voltage, this is pulse voltage divided by total target resistance, times the target cell resistance
 int heatSampleTime = 500; // How often the heater is sampled for PID, in milliseconds
 double pumpTarget = 0.45; // Target Nernst cell voltage
+int pumpSampleTime = 40; // How often the nernst cell is sampled for PID, in milliseconds (should be slower than the RC filter feeding the opamp)
+
+int speedOfLoop = 4; // How many millis it takes for loop to run, on Teensy++ 2.0, it's 4 millis
 
 //#define DEBUG       // DEBUG enables human readable logging and info messages
 //#define NO_SERIAL   // NO_SERIAL disables the CSV output of the controllers controls, a nice way to view this log is with OpenLogViewer - http://olv.diyefi.org/
@@ -29,7 +33,7 @@ double pumpTarget = 0.45; // Target Nernst cell voltage
 /*
   Pin Definitions
 */
-#ifdef BOARD_TEENSY_2PP
+#ifdef BOARD_TEENSYPP20
 const int PIN_VGND = PIN_F0; // Analog In - Level of the virtual ground
 const int PIN_VS = PIN_F1; // Analog In - Reads Nernst Cell voltage
 const int PIN_IPA = PIN_F2; // Analog In - Measures pump current via differential amp
@@ -37,7 +41,7 @@ const int POUT_VS = PIN_B6; // Digital Out - Pulses Nernst Cell to calculate int
 const int POUT_NARROW = PIN_F7; // Digital Out - Feeds voltage divider, 0-1v signal to ECU
 const int POUT_IP = PIN_B4; // Analog Out - Pump cell current output, keeps Nernst cell in stoich range
 const int POUT_HEATER = PIN_B5; // Analog Out - Heater control, keeps Nernst cell at right temperature
-
+const int POUT_WIDE = PIN_D1; // Analog Out - Wideband linear output
 
 
 const int PIN_RPM = -1;
@@ -60,15 +64,13 @@ const int PIN_RPM = -1;
 
 
 double heatIn, heatOut;
-PID heater(&heatIn, &heatOut, &heatTarget, 600, 200, 50, REVERSE);
+PID heater(&heatIn, &heatOut, &heatTarget, 800, 100, 1, REVERSE);
 double pumpIn, pumpOut;
-PID pump(&pumpIn, &pumpOut, &pumpTarget, 100, 1, 1, DIRECT);
+PID pump(&pumpIn, &pumpOut, &pumpTarget, 800, 100, 0.001, DIRECT);
 
-const int running = 10;
-
-RunningAverage vgndavg(running);
-RunningAverage ipaavg(running);
-RunningAverage nernstavg(running);
+RunningAverage vgndavg(1);
+RunningAverage ipaavg(pumpSampleTime/speedOfLoop*5);
+RunningAverage nernstavg(pumpSampleTime/speedOfLoop);
 
 float volts(int adc) {
   return adc / 1024.0 * 5.0;
@@ -110,7 +112,7 @@ const float lambdaLookup[LAMBDA_LOOKUP_SIZE] = {
    0.67, 1.43,
    0.94, 1.70,
    1.38, 2.42,
-   2.54, 9.99
+   2.54, 4.99
 };
 
 float lambda(float amps) {
@@ -167,6 +169,9 @@ void setup() {
   pinMode(POUT_NARROW, OUTPUT);
   digitalWrite(POUT_NARROW, 0);
   
+  pinMode(POUT_WIDE, OUTPUT);
+  analogWrite(POUT_WIDE, 0);
+  
   pinMode(POUT_VS, OUTPUT);
   digitalWrite(POUT_VS, 0);
   
@@ -181,6 +186,7 @@ void setup() {
   heater.SetSampleTime(heatSampleTime);
   heater.SetMode(AUTOMATIC);
   pump.SetOutputLimits(0, 250);
+  pump.SetSampleTime(pumpSampleTime);
   pump.SetMode(AUTOMATIC);
   
   // Start running
@@ -235,6 +241,12 @@ void loop() {
   } else {
     digitalWrite(POUT_NARROW, 1); // Rich
   }
+  
+  unsigned int widebandOut;
+  if (l < 0.65) widebandOut = 0;
+  else if (l > widebandTop) widebandOut = 255;
+  else widebandOut = ((l / widebandTop) - (0.65 / widebandTop)) * 255;
+  analogWrite(POUT_WIDE, widebandOut);
   
   
   serialLog(l, ipa, vgnd);
